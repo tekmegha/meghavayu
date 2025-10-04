280eb7a1c21eb57b831746672f442d426a75bf38
tekmegha <tekmegha@outlook.com>
2025-10-04 17:05:23 +0530

    supabse

--- tekmegha-fe/src/environments/environment.ts
+++ tekmegha-fe/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   supabase: {
     url: 'https://trfzoczdulcsmowxhhwi.supabase.co',
-    anonKey: process.env['SUPABASE_ANON_KEY'] || ''
+    anonKey: process.env['SUPABASE_ANON_KEY'] || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyZnpvY3pkdWxjc21vd3hoaHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5MTAwNTgsImV4cCI6MjA3NDQ4NjA1OH0.TPu5fK-4SozwruyFbMw4QC6wrzJl3Ge1WYkJmsePRrs'
   }
 };
